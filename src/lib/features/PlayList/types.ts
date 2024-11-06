8d8551c586856f29f762b5b01dde3e7ab09395b6
Zohaib887 <muhammad.zohaib@fortunesoft.pk>
2024-11-07 02:46:19 +0500

    add type playlist

--- src/lib/features/PlayList/types.ts
+++ src/lib/features/PlayList/types.ts
@@ -22,6 +22,7 @@ export interface AddSongResponse {
     user_id: number;
     created_at: string;
     updated_at: string;
+    media: MediaItem[]; // Make `media` optional
   }
   
   export interface PlaylistState {
