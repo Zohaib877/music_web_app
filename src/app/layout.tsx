9740e443c42fb9a37c2747c9a293ceea54ec414a
muhammadrahban <muhammadrahban4@gmail.com>
2024-12-18 04:19:21 +0500

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,11 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import "../styles/globals.css";
 import "../styles/globals.css";
 import StoreProvider from "./StoreProvider";
 import { Toaster } from "react-hot-toast";
 import AppWrapper from "@/components/AppWrapper";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Dhun",
@@ -21,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <body className={inter.className}>
+    <body className="font-inter">
       <Toaster position="top-right" reverseOrder={false} />
       <StoreProvider>
         <AppWrapper>{children}</AppWrapper>
